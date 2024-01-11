e6894f9a54c3cf8d717ab5052c5cac26775abb53
ZhangChenhuang <752306614@qq.com>
2024-01-11 17:42:09 +0800

    add: 新增用户中心模块

--- business-support/business-support-user/src/main/java/com/alice/UserApplication.java
+++ business-support/business-support-user/src/main/java/com/alice/UserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.annotation.Order;
@@ -19,14 +19,14 @@ import org.springframework.web.client.RestTemplate;
  * @DateTime 2022/9/23 0023 17:26
  */
 @EnableAsync
-@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"com.alice"})
 @EnableTransactionManagement
-public class ZuulApplication {
+public class UserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZuulApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 
     @Bean
